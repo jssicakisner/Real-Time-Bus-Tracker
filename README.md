c0a6ef0a501f33ada50fdc83d11d55eec3643f80
jssicakisner <94868170+jssicakisner@users.noreply.github.com>
2022-02-02 16:47:39 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Real-Time-Bus-Tracker
+# Real Time Bus Tracker
+
+This is a "Real Time" location of a random public bus in Boston, Massachusetts.
+
+## Installation
+
+Download the Zip file on this [link](https://github.com/jobdecrypt/Real-Time-Bus-Tracker/archive/refs/heads/main.zip) to get the files for the Real Time Bus Tracker Exercise.
+
+## Roadmap
+
+In the future, this could be turned into an app with features such as ETA for the passengers.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](./LICENSE)
---
+++ mapanimation.js
@@ -0,0 +1,40 @@
+//https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVzc2ljYS1raXNuZXIiLCJhIjoiY2t6M2hhZzE5MDZxYjJwcDRuZGhmbGE2OCJ9.1aiYz5GHGgzjT8n_oz14Zw';
+
+let followMarker = [];
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/satellite-streets-v11',
+  center: [-71.078786, 42.340253],
+  zoom: 14,
+});
+
+async function run() {
+  const locations = await getBusLocations();
+  let latLong = [];
+  latLong.push(locations[0].attributes.longitude); 
+  latLong.push(locations[0].attributes.latitude);
+
+  followMarker.push(locations[0].attributes.longitude);
+  followMarker.push(locations[0].attributes.latitude);
+  
+  new mapboxgl.Marker({ color: "red" }).setLngLat(latLong).addTo(map);
+
+  console.log(new Date());
+  console.log(latLong);
+
+  setTimeout(run, 3000);
+}
+
+async function getBusLocations() {
+  const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
+  const response = await fetch(url);
+  const json     = await response.json();
+
+  return json.data;
+}
+
+run();
+console.log(mapboxgl.Map.center);
